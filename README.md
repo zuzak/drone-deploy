3da669e520bf708ebd36e9dffd21098445e9409d
Leonard Martin <leonard.martin@gmail.com>
2018-04-05 12:06:27 +0100

    Docs

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # drone-deploy
+
+## Running
+
+Set the following environment variables:
+
+* `GITHUB_ORG`
+* `GITHUB_REPOS` - comma separated list of repo names
+* `DRONE_SERVER`
+* `DRONE_TOKEN`
+* `DEPLOY_ENVIRONMENTS` - comma separated list of environment names - defaults to `dev,preprod,prod`
+
+Start the server:
+
+```
+npm start
+```
+
+Visit [](http://localhost:3000).
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   server: process.env.DRONE_SERVER,
   token: process.env.DRONE_TOKEN,
-  environments: ['dev', 'preprod'],
+  environments: (process.env.DEPLOY_ENVIRONMENTS || 'dev,preprod,prod').split(',').map(s => s.trim()),
   org: process.env.GITHUB_ORG,
-  repos: (process.env.GITHUB_REPOS || '').split(',')
+  repos: (process.env.GITHUB_REPOS || '').split(',').map(s => s.trim()).filter(Boolean)
 };
